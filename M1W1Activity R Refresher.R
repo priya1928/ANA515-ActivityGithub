cd1aa9d7d4b87d084c160ce19712418f3748e7fb
priya1928 <priya.v.goyal12@gmail.com>
2021-07-01 22:38:24 -0400

    Add files via upload

---
+++ M1W1Activity R Refresher.R
@@ -0,0 +1,21 @@
+#flights: data frame
+flights
+airlines
+airports
+
+#methods to explore data frame
+View(flights)
+glimpse(flights)
+kable(flights)
+kable(airlines)
+glimpse(airlines)
+
+#Explore single variable in a data frame
+flights$year
+airlines$name
+
+#different types of var: identification(unique id) and measaurements
+glimpse(airports)
+
+#help  files
+?flights
